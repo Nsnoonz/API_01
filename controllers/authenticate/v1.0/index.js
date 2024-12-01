f58a60c04968d1de9cb2a570a8d3430cfc6060f0
Nsnoonz <noon.supavadee@gmail.com>
2024-12-02 01:52:38 +0700

    add 3

--- controllers/authenticate/v1.0/index.js
+++ controllers/authenticate/v1.0/index.js
@@ -3,11 +3,7 @@ import business from './business/business.js'
 const Main = {
   async fnDemo(req, res) {
     try {
-      /* const result = []  await business.bfnDemo(req, res) */
-      const result = {
-        Dev_USER: process.env.Dev_USER,
-        Dev_PASS: process.env.Dev_PASS
-      }
+      const result = await business.bfnDemo(req, res)
       res.success(result)
     } catch (error) {
       res.error(error.message, error.status)
