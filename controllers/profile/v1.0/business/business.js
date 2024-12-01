9df5df4d82857c7955e0309652e3f42a8e63198b
Nsnoonz <noon.supavadee@gmail.com>
2024-12-02 02:52:20 +0700

    test mail

--- controllers/profile/v1.0/business/business.js
+++ controllers/profile/v1.0/business/business.js
@@ -2,16 +2,25 @@ import { body } from 'express-validator'
 import nodemailer from 'nodemailer'
 import database from '../database/database.js'
 import validatorEndpoint from '../../../../middlewares/validatorResult.js'
-
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   port: 587,
   secure: false,
   auth: {
-      user: process.env.Mail_User,
-      pass: process.env.Mail_PASS,
+    user: process.env.Mail_User,
+    pass: process.env.Mail_PASS,
   },
 });
+
+const sendEmail = async (mailOptions) => {
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ' + info.response);
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
+
 const Business = {
   async bfnDemo(req, res) {
     try {
@@ -30,16 +39,17 @@ const Business = {
       const mailOptions = {
         from: `${req.body.mailname}`,
         to: process.env.Dev_Email,
-        subject:`Email From react-nsnoonz-dev : ${req.body.mailname} <${req.body.mailsender}>`,
+        subject: `Email From react-nsnoonz-dev : ${req.body.mailname} <${req.body.mailsender}>`,
         text: '',
         html: req.body.mailmessage,
       };
-      const result = transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            return false
-          }else{
-            return true
-          }
+      // const result = await transporter.sendMail(mailOptions, (error, info) => {
+      const result = await sendEmail(mailOptions, (error, info) => {
+        if (error) {
+          return false
+        } else {
+          return true
+        }
       });
     } catch (error) {
       throw error
