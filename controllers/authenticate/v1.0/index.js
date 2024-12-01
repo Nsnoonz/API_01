56e483bac91c9104789f55890f7e1f31cc5f66a0
Nsnoonz <noon.supavadee@gmail.com>
2024-12-02 01:26:16 +0700

    test

--- controllers/authenticate/v1.0/index.js
+++ controllers/authenticate/v1.0/index.js
@@ -3,7 +3,11 @@ import business from './business/business.js'
 const Main = {
   async fnDemo(req, res) {
     try {
-      const result = await business.bfnDemo(req, res)        
+      const result =  /* await business.bfnDemo(req, res) */
+        result = {
+          Dev_USER,
+          Dev_PASS
+        }
       res.success(result)
     } catch (error) {
       res.error(error.message, error.status)
@@ -11,7 +15,7 @@ const Main = {
   },
   async fnGenerateAccessToken(req, res) {
     try {
-      const result = await business.bfnGenerateAccessToken(req, res)        
+      const result = await business.bfnGenerateAccessToken(req, res)
       res.success(result)
     } catch (error) {
       res.error(error.message, error.status)
